11e7a5b4ab7c0022d871ca4cf38600941bd8d240
Dave <drregg6@gmail.com>
2018-04-14 13:55:07 -0400

    preliminary mousedown eventlistener

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,12 @@
-console.log('Link works!');
+var numbers = document.querySelectorAll('.number');
+
+numbers.forEach(function(number) {
+    number.addEventListener('mousedown', function(ev) {
+        console.log(this);
+        
+        this.classList.add('number-pressed');
+    });
+    number.addEventListener('mouseup', function(ev) {
+        this.classList.remove('number-pressed');
+    })
+});
