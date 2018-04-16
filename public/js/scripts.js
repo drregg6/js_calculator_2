9c227a789c76ca7bd5f2748c1b026a66b052a667
Dave <drregg6@gmail.com>
2018-04-16 16:12:50 -0400

    playing with dimensions, grabbing each button individually

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,9 +1,16 @@
-var numbers = document.querySelectorAll('.number');
+let numbers = document.querySelectorAll('.number');
+let zeroButton = document.querySelector('#zero');
+let oneButton = document.querySelector('#one');
+let twoButton = document.querySelector('#two');
+let threeButton = document.querySelector('#three');
+let fourButton = document.querySelector('#four');
+let fiveButton = document.querySelector('#five');
+let sixButton = document.querySelector('#six');
 
+
+// button effects
 numbers.forEach(function(number) {
     number.addEventListener('mousedown', function(ev) {
-        console.log(this);
-        
         this.classList.add('number-pressed');
     });
     number.addEventListener('mouseup', function(ev) {
