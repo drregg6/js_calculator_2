aac9b5443ab65630ca4458ea2c8d74af2a495db8
Dave <drregg6@gmail.com>
2018-04-18 12:33:57 -0400

    added operations and changed the color of equals

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,31 +1,24 @@
 let numbers = document.querySelectorAll('.number');
-let zeroButton = document.querySelector('#zero');
-let oneButton = document.querySelector('#one');
-let twoButton = document.querySelector('#two');
-let threeButton = document.querySelector('#three');
-let fourButton = document.querySelector('#four');
-let fiveButton = document.querySelector('#five');
-let sixButton = document.querySelector('#six');
-let sevenButton = document.querySelector('#seven');
-let eightButton = document.querySelector('#eight');
-let nineButton = document.querySelector('#nine');
 
 let screen = document.querySelector('.screen');
 let displayStr = screen.firstChild.nextSibling;
 
-// button effects
+
+
 numbers.forEach(function(number) {
+    // button pressdown effect
     number.addEventListener('mousedown', function(ev) {
         this.classList.add('number-pressed');
     });
     number.addEventListener('mouseup', function(ev) {
         this.classList.remove('number-pressed');
     });
+    // screen display effect
     number.addEventListener('click', function(ev) {
         let pressedStr = this.firstChild.nextSibling.textContent;
         let pressedNum = parseInt(pressedStr);
-        console.log(pressedStr);
-        console.log(pressedNum);
+        
+        
         
         if (displayStr.textContent === '0') {
             displayStr.textContent = pressedStr;
@@ -34,6 +27,39 @@ numbers.forEach(function(number) {
         }
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let zeroButton = document.querySelector('#zero');
+//let oneButton = document.querySelector('#one');
+//let twoButton = document.querySelector('#two');
+//let threeButton = document.querySelector('#three');
+//let fourButton = document.querySelector('#four');
+//let fiveButton = document.querySelector('#five');
+//let sixButton = document.querySelector('#six');
+//let sevenButton = document.querySelector('#seven');
+//let eightButton = document.querySelector('#eight');
+//let nineButton = document.querySelector('#nine');
+
+
+
 //zeroButton.addEventListener('click', ev => console.log('Zero!'));
 //oneButton.addEventListener('click', function(ev) {
 //    let num = parseInt(this.firstChild.nextSibling.innerText);
