4f98db27ac00d0712ff75014177a11578e954af6
Dave <drregg6@gmail.com>
2018-04-23 10:28:24 -0400

    changed var names and added comments

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,10 @@
 /*
 
-TODO: keypress should effect currentNumber and displayScreen
+TODO: keypress should effect currentNum and displayScreen
 TODO: figure out how to get the plus and multiply keypress to work
             (it doesn't work with the shift + equals and shift + eight)
 TODO: code can be cleaned
 TODO: send helpers into a new file
-TODO: i should comment this code
 
 */
 
@@ -39,37 +38,42 @@ let isDividing = false;
 let isEqualing = false;
 
 // data storage
-let storedNumber = 0;
-let currentNumber = 0;
+let storedNum = 0;
+let currentNum = 0;
 
 
 
 // event listeners
 
 buttons.forEach(function(button) { // changing the css
-    button.addEventListener('mousedown', pressingDown, false);
-    button.addEventListener('mouseup', pressingUp, false);
+    button.addEventListener('mousedown', pressDown, false);
+    button.addEventListener('mouseup', pressUp, false);
 });
 
 numbers.forEach(function(number) { // updating data and display
-    number.addEventListener('click', updateCurrentNumber, false);
+    number.addEventListener('click', updatecurrentNum, false);
 });
 
 operations.forEach(function(operation) { // performing operations
     operation.addEventListener('click', clickOperation, false);
 });
 
-equals.addEventListener('click', equaling);
+equals.addEventListener('click', equaling); // completing operations
 
 // event listeners for deleting
-del.addEventListener('click', backspace);
-clear.addEventListener('click', resetCalculator);
+del.addEventListener('click', backspace); // go back one digit
+clear.addEventListener('click', resetCalculator); // start anew
 
 // event listeners for special keys
-plusMinus.addEventListener('click', posToNeg);
-decimal.addEventListener('click', floatingNumber);
+plusMinus.addEventListener('click', posToNeg); // swap between positive and negative
+decimal.addEventListener('click', floatingNumber); // use decimals
 
 // listening for keypress on the window
+/*
+
+broken
+
+
 window.addEventListener('keydown', function(ev) {
     console.log(ev);
     let keycode = ev.which;
@@ -78,7 +82,7 @@ window.addEventListener('keydown', function(ev) {
     // if .button exists in the .html, change the css
     if (buttonPressed !== null) {
         console.log(buttonPressed);
-        updateCurrentNumber(buttonPressed);
+        updatecurrentNum(buttonPressed);
         buttonPressed.classList.add('button-pressed');
     }
 });
@@ -92,51 +96,51 @@ window.addEventListener('keyup', function(ev) {
         buttonPressed.classList.remove('button-pressed');
     }
 });
-
+*/
 
 
 // event listener methods
 
-function updateCurrentNumber(buttonPressed) { // this can probably be broken apart somewhere
+function updatecurrentNum(buttonPressed) { // this can probably be broken apart somewhere
     let displayStringLen = displayString.textContent.length;
     let pressedString = this.firstChild.nextSibling;
-    let pressedInt = parseFloat(pressedString.textContent);
+    let pressedNum = parseFloat(pressedString.textContent);
 
     if (displayString.textContent.length === 18) {
         return;
     } else if (isEqualing) {
         isEqualing = false;
-        storedNumber = 0;
+        storedNum = 0;
         displayString.textContent = pressedString.textContent;
-        currentNumber = pressedInt;
-    } else if (currentNumber === 0) {
+        currentNum = pressedNum;
+    } else if (currentNum === 0) {
         displayString.textContent = pressedString.textContent;
-        currentNumber = pressedInt;
+        currentNum = pressedNum;
     } else if (displayString.textContent === '0.') {
-        console.log(pressedInt);
-        currentNumber = parseFloat(displayString.textContent + pressedString.textContent);
-        displayString.textContent = "" + currentNumber;
+        console.log(pressedNum);
+        currentNum = parseFloat(displayString.textContent + pressedString.textContent);
+        displayString.textContent = "" + currentNum;
     } else {
-        currentNumber = parseFloat(displayString.textContent + pressedString.textContent);
-        displayString.textContent = "" + currentNumber;
+        currentNum = parseFloat(displayString.textContent + pressedString.textContent);
+        displayString.textContent = "" + currentNum;
     }
     
     console.log({
-        storedNumber: storedNumber,
-        currentNumber: currentNumber
+        storedNum: storedNum,
+        currentNum: currentNum
     });
 }
 
 function clickOperation(ev) {
     
-    if (storedNumber === 0) { // beginning an operation
-        storedNumber = currentNumber;
+    if (storedNum === 0) { // beginning an operation
+        storedNum = currentNum;
     } else { // continuing an operation
         operate();
     }
-    currentNumber = 0;
+    currentNum = 0;
     
-    // wave a flag
+    // wave a flag to detect what kind of operation to perform
     switch(ev.target.id) {
             case 'add':
                 isAdding = true;
@@ -154,44 +158,53 @@ function clickOperation(ev) {
 }
 
 function equaling(ev) {
+    // the same as operating, but has a different effect on operation and number clicks
+    // so I need a flag to keep track of equaling
     operate();
     isEqualing = true;
 }
 
 function posToNeg() {
-    if (storedNumber !== 0 && currentNumber === 0 || isEqualing) {
+    if (storedNum !== 0 && currentNum === 0 || isEqualing) {
+        // cannot change positive/negative of the solution in the middle of an operation
         return;
-    } else if (currentNumber > 0 || currentNumber < 0) {
-        currentNumber = currentNumber * -1;
-        displayString.textContent = "" + currentNumber;
+    } else if (currentNum > 0 || currentNum < 0) {
+        currentNum = currentNum * -1;
+        displayString.textContent = "" + currentNum;
     }
     
     console.log({
-        currentNumber: currentNumber,
-        storedNumber: storedNumber
-    })
+        currentNum: currentNum,
+        storedNum: storedNum
+    });
 }
 
 function floatingNumber() {
-    if (displayString.textContent.indexOf(".") !== -1 && currentNumber !== 0) {
+    // cannot add a decimal if a decimal already exists
+    // and if user is not in the middle of an operation that already has a decimal
+    // ie- displayString.textContent = "2.4"
+    //      user clicks #decimal to create "0.4"
+    //      but since displayString.textContent.indexOf(".") !== -1
+    //      #decimal cannot be used
+    if (displayString.textContent.indexOf(".") !== -1 && currentNum !== 0) {
         return;
-    } else if (currentNumber === 0) {
+    } else if (currentNum === 0) {
         displayString.textContent = '0.';
-        currentNumber = NaN;
+        currentNum = NaN; // temporarily changes to NaN so it doesn't interfere with .operations
     } else {
         displayString.textContent = displayString.textContent + '.';
     }
     
     console.log({
-        currentNumber: currentNumber,
-        storedNumber: storedNumber
-    })
+        currentNum: currentNum,
+        storedNum: storedNum
+    });
 }
 
-function pressingDown(ev) {
+function pressDown(ev) {
     this.classList.add('button-pressed');
 }
-function pressingUp(ev) {
+function pressUp(ev) {
     this.classList.remove('button-pressed');
 }
 
@@ -199,19 +212,19 @@ function backspace() {
     let displayStringLen = displayString.textContent.length;
     
     if (displayStringLen === 1) {
-        currentNumber = 0;
-        displayString.textContent = currentNumber.toString();
+        currentNum = 0;
+        displayString.textContent = currentNum.toString();
     } else {
-        currentNumber = backspace(displayString.textContent);
-        displayString.textContent = "" + currentNumber;
+        currentNum = backspace(displayString.textContent);
+        displayString.textContent = "" + currentNum;
     }
 }
 
 function resetCalculator() {
     resetFlags();
-    storedNumber = 0;
-    currentNumber = 0;
-    displayString.textContent = currentNumber.toString();
+    storedNum = 0;
+    currentNum = 0;
+    displayString.textContent = currentNum.toString();
 }
 
 
@@ -223,19 +236,19 @@ function backspace(str) {
 }
 
 function operate() {
-    if (currentNumber !== 0) {
+    if (currentNum !== 0) {
         if (isDividing) {
-            storedNumber = dividing(storedNumber, currentNumber);
+            storedNum = dividing(storedNum, currentNum);
         } else if (isSubtracting) {
-            storedNumber = subtracting(storedNumber, currentNumber);
+            storedNum = subtracting(storedNum, currentNum);
         } else if (isAdding) {
-            storedNumber = adding(storedNumber, currentNumber);
+            storedNum = adding(storedNum, currentNum);
         } else if (isMultiplying) {
-            storedNumber = multiplying(storedNumber, currentNumber);
+            storedNum = multiplying(storedNum, currentNum);
         }
     }
     resetFlags(); // flags restored as clickevent continues
-    displayString.textContent = "" + storedNumber;
+    displayString.textContent = "" + storedNum;
 }
 
 function adding(a, b) {
